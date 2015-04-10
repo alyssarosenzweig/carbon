ca535835a0d206f9a6d9ebe6e1437eac762d9f39
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-04-10 12:28:35 -0400

    WebGL context creation

---
+++ preamble.js
@@ -0,0 +1,32 @@
+/* ctx is an object in the form of:
+{
+  container: document.body
+}.
+
+function carbon returns a modified ctx
+*/
+
+var carbon = function(ctx) {
+  /* initialize rending context */
+
+  ctx.initWebGL = function (width, height) {
+    // init canvas
+    var canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    ctx.container.appendChild(canvas);
+
+    // detect WebGL
+    var gl = (canvas.getContext("webgl") ||
+              canvas.getContext("experimental-webgl"));
+
+    if(!gl) {
+      console.error("WebGL is not supported in this browser");
+      console.error("Please upgrade your browser.");
+      return;
+    }
+
+    // save context
+    ctx.gl = gl;
+  }
+};
