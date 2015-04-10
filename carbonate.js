58236f8e92bd1de6d0da83ab96228b8aef791da7
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-04-10 17:57:21 -0400

    return; expressions

--- carbonate.js
+++ carbonate.js
@@ -13,6 +13,8 @@ fs.readFile(process.argv[2], function(err, content) {
 
   var functionList = [];
 
+  var globalContext = {};
+
   tokens.map(function(globalStatement) {
     if(globalStatement[0] == "func") {
       functionList.push(globalStatement[2]);
@@ -28,11 +30,30 @@ fs.readFile(process.argv[2], function(err, content) {
       // function declaration
       output.push("function "+globalStatement[2]+"("+flatParams.join(",")+")"+"{");
 
+      localContext = {};
+
+      // parameter coercion
       globalStatement[3].map(function(p) {
         if(p.length == 2) {
           // add a parameter coercion
           output.push(parameterCoercion(p[0], p[1]));
+
+          // remember in context
+          localContext[p[1]] = {
+            type: p[0],
+            name: p[1],
+            source: "param"
+          }
         }
+      })
+
+      // function body compilation
+      globalStatement[4].map(function(statement) {
+        if(statement[0] == "return") {
+          var c = compileExpression(statement[1], localContext, globalContext);
+          output.push(returnCoercion(c, globalStatement[1]));
+        }
+        console.log(statement);
       })
 
       // end function
@@ -65,4 +86,48 @@ function parameterCoercion(type, name) {
   console.log(name);
 
   process.exit(0);
+}
+
+function returnCoercion(value, type) {
+  // TODO: more types
+  if(type == "int") {
+    return "return ("+value+")|0;";
+  }
+
+  console.error("Unknown return type:");
+  console.log(type);
+  console.log(name);
+
+  process.exit(0);
+}
+
+function compileExpression(exp, localContext, globalContext) {
+  if(exp[0] == "/") {
+    var leftOp = compileExpression(exp[1], localContext, globalContext),
+        rightOp = compileExpression(exp[2], localContext, globalContext);
+
+    return "("+leftOp+"/"+rightOp+")";
+  } else if(exp[0] == "*") {
+    var leftOp = compileExpression(exp[1], localContext, globalContext),
+        rightOp = compileExpression(exp[2], localContext, globalContext);
+
+    return "("+leftOp+"*"+rightOp+")";
+  } else if(exp[0] == "-") {
+    var leftOp = compileExpression(exp[1], localContext, globalContext),
+        rightOp = compileExpression(exp[2], localContext, globalContext);
+
+    return "("+leftOp+"-"+rightOp+")";
+  } else if(exp[0] == "+") {
+    var leftOp = compileExpression(exp[1], localContext, globalContext),
+        rightOp = compileExpression(exp[2], localContext, globalContext);
+
+    return "("+leftOp+"+"+rightOp+")";
+  } else if( (exp * 1) == exp) {
+    return exp;
+  } else if( localContext[exp] || globalContext[exp]) {
+    return exp;
+  }
+
+  console.log(exp);
+  return "";
 }
