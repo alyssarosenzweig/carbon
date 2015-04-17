cfe8ee9b1e4b050cd813ef8b497cc10a3657013a
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-04-17 17:45:57 -0400

    Texture cutting for spritesheets

--- fountain.js
+++ fountain.js
@@ -111,7 +111,7 @@ var fountain = function(ctx) {
     ctx.t = rbuffer;
 
     for(var i = 0; i < spriteCount; ++i) {
-      var sindex = 1 + (i * 4); // starting index for sprite
+      var sindex = 1 + (i * 8); // starting index for sprite
       var xv = ctx.float64View[sindex+0], // x coord
           yv = ctx.float64View[sindex+1], // y coord
           wv = ctx.float64View[sindex+2], // width
@@ -135,13 +135,33 @@ var fountain = function(ctx) {
     var arr = new Float32Array(12 * spriteCount);
 
     for(var i = 0; i < spriteCount; ++i) {
+      var sindex = 1 + (i * 8); // starting index for sprite
+      var x1 = ctx.float64View[sindex+4], // tex x1
+          y1 = ctx.float64View[sindex+5], // tex y1
+          x2 = ctx.float64View[sindex+6], // tex x2
+          y2 = ctx.float64View[sindex+7]; // tex y2
+
+
       var ind = i * 12;
-      arr[ind+2] = 1;
-      arr[ind+5] = 1;
-      arr[ind+6] = 1;
-      arr[ind+8] = 1;
-      arr[ind+9] = 1;
-      arr[ind+11] = 1;
+
+      arr[ind+0] = x1;
+      arr[ind+1] = y1;
+
+      arr[ind+2] = x2;
+      arr[ind+3] = y1;
+
+      arr[ind+4] = x1;
+      arr[ind+5] = y2;
+
+
+      arr[ind+6] = x2;
+      arr[ind+7] = y1;
+
+      arr[ind+8] = x2;
+      arr[ind+9] = y2;
+
+      arr[ind+10] = x1;
+      arr[ind+11] = y2;
     }
 
     ctx.gl.bufferData(ctx.gl.ARRAY_BUFFER, arr, ctx.gl.STATIC_DRAW);
--- sprites.c
+++ sprites.c
@@ -4,11 +4,19 @@ double* px = 8;
 double* py = 16;
 double* pw = 24;
 double* ph = 32;
+double* ptx1 = 40;
+double* pty1 = 48;
+double* ptx2 = 56;
+double* pty2 = 64;
 
-double* ox = 40;
-double* oy = 48;
-double* ow = 56;
-double* oh = 64;
+double* ox = 72;
+double* oy = 80;
+double* ow = 88;
+double* oh = 96;
+double* otx1 = 104;
+double* oty1 = 112;
+double* otx2 = 120;
+double* oty2 = 128;
 
 int direction = 0;
 
@@ -19,6 +27,14 @@ void init() {
   *ph = 0.4;
   *ow = *pw;
   *oh = *ph;
+
+  *ptx2 = 1.0;
+  *pty2 = 1.0;
+
+  *otx1 = 0.5;
+  *oty1 = 0;
+  *otx2 = 1.0;
+  *oty2 = 1.0;
 }
 
 void loop() {
