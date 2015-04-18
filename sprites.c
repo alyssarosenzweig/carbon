7a90d368e7381cc94a9a4130cad27070a09fc333
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-04-18 11:26:04 -0400

    Convert Sprite example to use structptr

--- sprites.c
+++ sprites.c
@@ -13,20 +13,11 @@ struct Sprite {
 };
 
 structptr(Sprite) heart = 8;
+structptr(Sprite) smile = 80;
 
 double* isTouching = 65536;
 double* touchX = 65544;
 double* touchY = 65552;
-
-double* ox = 80;
-double* oy = 88;
-double* ow = 96;
-double* oh = 104;
-double* otx1 = 112;
-double* oty1 = 120;
-double* otx2 = 128;
-double* oty2 = 136;
-double* oa = 144;
 
 int direction = 0;
 
@@ -35,21 +26,18 @@ void init() {
 
   heart->w = 0.4;
   heart->h = 0.4;
-  *ow = 0.4;
-  *oh = 0.4;
+  smile->w = 0.4;
+  smile->h = 0.4;
 
   heart->tx1 = 0.0;
   heart->ty1 = 0.0;
   heart->tx2 = 0.5;
   heart->ty2 = 0.5;
 
-  *otx1 = 0.5;
-  *oty1 = 0.0;
-  *otx2 = 1.0;
-  *oty2 = 0.5;
-
-  *oa = 0.0;
-  heart->a = 0.0;
+  smile->tx1 = 0.5;
+  smile->ty1 = 0.0;
+  smile->tx2 = 1.0;
+  smile->ty2 = 0.5;
 }
 
 void loop() {
@@ -57,22 +45,22 @@ void loop() {
   heart->y = *touchY;
 
   if(direction == 0) {
-    *ox -= 0.01;
-    *oy -= 0.01;
+    smile->x -= 0.01;
+    smile->y -= 0.01;
 
     heart->a += 0.01;
   } else {
-    *ox += 0.01;
-    *oy += 0.01;
+    smile->x += 0.01;
+    smile->y += 0.01;
 
     heart->a -= 0.01;
   }
 
-  if(*ox > 2) {
+  if(smile->x > 2) {
     direction = 0;
   }
 
-  if(*ox < -1.9) {
+  if(smile->x < -1.9) {
     direction = 1;
   }
 }
